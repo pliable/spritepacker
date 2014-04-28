e3ed6acc7db7ef6b3cc88c4b9c31f3cdaacc117f
Steve Choo <pliable@gmail.com>
2014-04-27 18:14:56 -0700

    forgot to link library whoops

--- Makefile
+++ Makefile
@@ -1,10 +1,11 @@
 CC = gcc
 CFLAGS = -Wall -Wextra -std=c99
+LFLAGS = -lfreeimage
 
 all: spacker
 
 spacker: main.c
-	$(CC) $(CFLAGS) -o spacker main.c
+	$(CC) $(CFLAGS) -o spacker main.c $(LFLAGS)
 
 clean:
 	rm -f ./*.o spacker
--- main.c
+++ main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <sys/types.h>
+#include <dirent.h>
 #include "FreeImage.h"
 
+/* #defines to remove magic numbers
+   since we all hate that
+   */
+#define DIRECTORY 1
 #define NEEDED_ARGS 3
 
 /*
@@ -17,12 +23,21 @@
    * @author Steve Choo
 */
 int main(int argc, char *argv[]) {
-   bool test = true;
+   FreeImage_Initialise(false);
+   DIR* currDir;
+   struct dirent* currEntry;
    /* skipping robust checking because 2 args */
    if(argc != NEEDED_ARGS) {
       fprintf(stderr, "Use: ./spritepacker inputFolder outputName\n");
       exit(EXIT_FAILURE);
    }
 
+   /* we open dir */
+   if((currDir = opendir(argv[DIRECTORY])) == NULL) {
+      perror("opendir");
+      exit(EXIT_FAILURE);
+   }
+
+   FreeImage_DeInitialise();
    return 0;
 }
