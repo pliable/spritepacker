49bfc6a03480a0cc4a32a939fb30a1af0b867556
Steve Choo <pliable@gmail.com>
2014-06-20 20:23:54 -0700

    moved freeimage.h into helper.h

--- helper.c
+++ helper.c
@@ -23,7 +23,6 @@
 #include <string.h>
 #include <sys/types.h>
 #include <math.h>
-#include "FreeImage.h"
 #include "helper.h"
 
 /* Do i really need to pass in FIBITMAP** canvas? I'm not even using it in main so wtf am i doing */
